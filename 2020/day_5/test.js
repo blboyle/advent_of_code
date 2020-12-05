40ebd02feaed88f4c5169b9efb82f4755c2a9440
Brendan Boyle <bboyle@Brendans-MacBook-Pro-16.local>
2020-12-05 11:04:48 -0500

    part 2 and refactoring

--- 2020/day_5/test.js
+++ 2020/day_5/test.js
@@ -1,93 +1,105 @@
-const {answer, row, column, seatId} = require('./script');
+const {highestNumber, row, column, seatId, missing} = require('./script');
 
-describe('test', () => {
+describe('day5', () => {
 
-    test("answer", async ()  => {
-        const myAnswer = await answer();
-        expect(myAnswer).toBe(888);
+    describe('part a', () => {
+
+        test("answer", async ()  => {
+            const myAnswer = await highestNumber();
+            expect(myAnswer).toBe(888);
+        })
+
+        describe("FBFBBFFRLR", () => {
+            test("row", async () => {
+                const input = "FBFBBFFRLR";
+                const output = await row(input);
+                expect(output).toBe(44);
+            })
+        
+            test("column", async () => {
+                const input = "FBFBBFFRLR";
+                const output = await column(input);
+                expect(output).toBe(5);
+            })
+        
+            test("seatid", async () => {
+                const [r,c] = [44 ,5];
+                const output = await seatId(r,c);
+                expect(output).toBe(357);
+            })
+        })
+
+        describe("BFFFBBFRRR", () => {
+        
+            test("row", async ()  => {
+                const input = "BFFFBBFRRR";
+                const output = await  row(input);
+                expect(output).toBe(70);
+            })
+        
+            test("column", async () => {
+                const input = "BFFFBBFRRR";
+                const output = await column(input);
+                expect(output).toBe(7);
+            })
+        
+            test("seatid", async () => {
+                const [r,c] = [70,7];
+                const output = await seatId(r,c);
+                expect(output).toBe(567);
+            })
+        })
+
+        describe("FFFBBBFRRR", () => {
+            test("row", async () => {
+                const input = "FFFBBBFRRR";
+                const output = await row(input);
+                expect(output).toBe(14);
+            })
+        
+            test("column", async () => {
+                const input = "FFFBBBFRRR";
+                const output = await column(input);
+                expect(output).toBe(7);
+            })
+        
+            test("seatid", async () => {
+                const [r,c] = [14,7];
+                const output = await seatId(r,c);
+                expect(output).toBe(119);
+            })
+        })
+
+        describe("BBFFBBFRLL", () => {
+            test("row", async () => {
+                const input = "BBFFBBFRLL";
+                const output = await row(input);
+                expect(output).toBe(102);
+            })
+        
+            test("column", async () => {
+                const input = "BBFFBBFRLL";
+                const output = await column(input);
+                expect(output).toBe(4);
+            })
+        
+            test("seatid", async () => {
+                const [r,c] = [102, 4];
+                const output = await seatId(r,c);
+                expect(output).toBe(820);
+            })
+        })
+
     })
 
-    describe("FBFBBFFRLR", () => {
-        test("row", async () => {
-            const input = "FBFBBFFRLR";
-            const output = await row(input);
-            expect(output).toBe(44);
-        })
-    
-        test("column", async () => {
-            const input = "FBFBBFFRLR";
-            const output = await column(input);
-            expect(output).toBe(5);
-        })
-    
-        test("seatid", async () => {
-            const input = [44 ,5];
-            const output = await seatId(input);
-            expect(output).toBe(357);
-        })
-    })
+    describe('part a', () => {
 
-    describe("BFFFBBFRRR", () => {
-      
-        test("row", async ()  => {
-            const input = "BFFFBBFRRR";
-            const output = await  row(input);
-            expect(output).toBe(70);
+        test('my seat', async () => {
+            const mySeatId = await missing();
+            expect(mySeatId).toBe(522);
         })
-    
-        test("column", async () => {
-            const input = "BFFFBBFRRR";
-            const output = await column(input);
-            expect(output).toBe(7);
-        })
-    
-        test("seatid", async () => {
-            const input = [70,7];
-            const output = await seatId(input);
-            expect(output).toBe(567);
-        })
-    })
 
-    describe("FFFBBBFRRR", () => {
-        test("row", async () => {
-            const input = "FFFBBBFRRR";
-            const output = await row(input);
-            expect(output).toBe(14);
-        })
-    
-        test("column", async () => {
-            const input = "FFFBBBFRRR";
-            const output = await column(input);
-            expect(output).toBe(7);
-        })
-    
-        test("seatid", async () => {
-            const input = [14,7];
-            const output = await seatId(input);
-            expect(output).toBe(119);
-        })
-    })
+    });
 
-    describe("BBFFBBFRLL", () => {
-        test("row", async () => {
-            const input = "BBFFBBFRLL";
-            const output = await row(input);
-            expect(output).toBe(102);
-        })
-    
-        test("column", async () => {
-            const input = "BBFFBBFRLL";
-            const output = await column(input);
-            expect(output).toBe(4);
-        })
-    
-        test("seatid", async () => {
-            const input = [102, 4];
-            const output = await seatId(input);
-            expect(output).toBe(820);
-        })
-    })
-
-    
 
 });
