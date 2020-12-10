cc4f9e4c994e2b86984b27e349eadc00b3d883f9
Brendan Boyle <bboyle@Brendans-MacBook-Pro-16.local>
2020-12-09 19:37:26 -0500

    this suckks

--- 2020/day_7/script.js
+++ 2020/day_7/script.js
@@ -1,98 +1,93 @@
 const {getInputArray} = require('../utilities.js')
 
 let listOfBagTypes;
-let bagAssociation;
-let reachableFrom = [];
+let listOfBags;
 
 const howManyBags = async (needle, path) => {
 
     const data = await getInputArray(`${__dirname}/${path}`);
-    listOfBagTypes = typesOfBags(data)
-    bagAssociation = buildBagAssociation(data);
+    listOfBags = formatBags(data)
+    count = countPossibleBags(needle);
+    total = countTotalNumber(needle);
 
-    return countPossibleBags(needle);
+    return [count,total];
 }
 
 const countPossibleBags = (needle) => {
 
-    const needleIndex = listOfBagTypes.indexOf(needle);
+    const foundColours = listOfBags
+        .filter((bag) => bag.children)
+        .filter((bag) => bag.name !== needle)
+        .filter((bag) => {
+            return bag.children.some((child) => {
+                return child.name == needle;
+            })
+        });
 
-    const checks = bagAssociation.map((_, i) => {
-
-        if (i < 200000){
-            return canIReach(needleIndex, i);
-        }
-
+    const all =  [
+        ...foundColours,
+        ...foundColours.map((bag) => {
+            return countPossibleBags(bag["name"])
+        }).flat()
+    ]
+    .filter((value, index, array) => {
+        return array.indexOf(value) === index;
     });
 
-    const answer = checks.filter((item) => item);
 
-    console.log(reachableFrom);
-
-    return answer.length;
+    return all.length;
 
 }
 
-const canIReach = (needle, i) => {
+const countTotalNumber = (needle) => {
 
-    if (needle == i) {
-        return false;
-    }
+    const foundBag = listOfBags
+        .find(bag => {
+            return bag.name == needle;
+        })
 
-    let bagsToCheck = [i];
-    let checkedBags = [];
-    let reachable = false;
+    console.log({foundBag});
 
-    console.log('---', i)
-
-    while (bagsToCheck.length > 0) {
-        const bag = bagsToCheck.shift();
- 
-        for (item of bagAssociation[bag]) {
-            if (!checkedBags.includes(item)) {
-                bagsToCheck.push(item);
-            }
-        }
-        
-        if (bagAssociation[bag].includes(needle) || reachableFrom.includes(bag)) {
-            console.log(">>>");
-            bagsToCheck = [];
-            reachable = true;
-        }
-
-        checkedBags.push(bag);
-
-    }
-
-    if (reachable) {
-        reachableFrom.push(i);
-    }
-
-    return reachable;
-
+    if (foundBag.children.length === 0) {
+        return 1;
+      }
+    
+    
+    return foundBag.children.reduce((sum, child) => {
+        console.log({child},parseInt(child.number, 10))
+        return sum + parseInt(child.number, 10) * countTotalNumber(child.name);
+    }, 1);
 }
 
 
-const buildBagAssociation = (data) => data.map((rule) => {
 
-    const children = listOfBagTypes.map((type) => {
-        const regex = `${type}`;
-        const child = rule.match(regex);
-        if (child){
-            return child[0];
-        }
-    }).filter((item) => item != null).splice(1);
+const formatBags = (data) => data.map((rule) => {
 
-    return getBagIndexOf(children);
+    const name = rule.match(/(.+) bags contain/)[1]
+    const availableColors = rule.split(' bags contain ')[1]
+        .replace('.', '')
+        .replace(' bags', '')
+        .split(', ');
+    
+    
+    return {
+        name,
+        children:  availableColors
+            .filter((color) => color !== 'no other')
+            .map((color) => ({
+                number: color.split(' ')[0],
+                name: color.split(' ')[1] + ' ' + color.split(' ')[2],
+            })),
 
-});
+    }
+}).filter(bag => bag.children !== null);
 
 const getBagIndexOf = (bagNames) => bagNames.map((item) => listOfBagTypes.indexOf(item))
 
 const typesOfBags = (data) => data.map((rule) => rule.match(/(.+) bags contain/)[1]);
 
 const run = async () => {
-    const answer = await howManyBags('shiny gold', 'test');
+    const answer = await howManyBags('shiny gold', '');
     console.log(answer);
 }
 
