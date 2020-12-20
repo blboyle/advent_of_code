a5dee0b097edf9c6039f5aaf0b44a02b3449f2eb
Brendan Boyle <bboyle@Brendans-MacBook-Pro-16.local>
2020-12-20 12:58:58 -0500

    day 12

--- 2020/day_12/script.js
+++ 2020/day_12/script.js
@@ -1,31 +1,138 @@
 const {getInputArray, numberSort} = require('../utilities.js')
 
+
+let cardinals = ["N", "E", "S", "W"];
 let directionIndex = 1;
 let x = 0;
 let y = 0;
-let directions = ["N", "E", "S", "W"];
+
+let waypointX = 10;
+let waypointY = 1;
 
 let location = {
-    direction: directions[directionIndex],
+    direction: cardinals[directionIndex],
     location: [x,y]
 }
 
+let wayPointLocation = [waypointX, waypointY];
+
+const reset = () => {
+    directionIndex = 1;
+    x = 0;
+    y = 0;
+    waypointX = 10;
+    waypointY = 1;
+
+}
+
 const findAnswer = async (path) => {
+
+    reset();
 
     const data = await getInputArray(`${__dirname}/${path}`);
     const directions = makeDirections(data);
     const endLocation = useDirections(directions);
 
     const answer = Math.abs(endLocation[0]) + Math.abs(endLocation[1]);
-    console.log(answer);
+
+    reset();
+
+    const waypointInfo = withWaypoint(directions);
+
+    const secondAnswer = Math.abs(waypointInfo[0]) + Math.abs(waypointInfo[1]);
  
-    return [answer];
+    return [answer, secondAnswer];
+
+}
+
+const withWaypoint = directions => {
+    directions.forEach(direction => {
+
+        if (direction[0] == "R" || direction[0] == "L") {
+            rotateWaypoint(direction)
+        } else {
+            moveWaypoint(direction);
+        }
+        
+        update();
+    })
+
+    return location.location;
+}
+
+const rotateWaypoint = ([turn, amount]) => {
+
+    let turns = amount / 90;
+
+    if (turns > 3) {
+        turns = turns % 4;
+    }
+
+    let tempX = waypointX;
+    let tempY = waypointY;
+
+    switch(turns){
+        case 1:
+            if (turn == "R") {
+                waypointX = tempY;
+                waypointY = -tempX;
+            }
+            if (turn == "L") {
+                waypointX = -tempY;
+                waypointY = tempX;
+            }
+            break;
+        case 2:
+            waypointX = -tempX;
+            waypointY = -tempY;
+            break;
+        case 3:
+            if (turn == "R") {
+                waypointX = -tempY;
+                waypointY = tempX;
+            }
+            if (turn == "L") {
+                waypointX = tempY;
+                waypointY = -tempX;
+            }
+            break;
+    }
+
+}
+
+const moveWaypoint = ([direction, amount]) => {
+
+    switch (direction){
+        case "N":
+            waypointY += Number(amount)
+            break;
+        case "E":
+            waypointX += Number(amount)
+            break;
+        case "S":
+            waypointY -= Number(amount)
+            break;
+        case "W":
+            waypointX -= Number(amount)
+            break;
+        case "F":
+            moveTowardWaypoint(amount);
+            break;
+    }
+
+}
+
+const moveTowardWaypoint = amount => {
+
+    x = x + (wayPointLocation[0] * amount);
+    y = y + (wayPointLocation[1] * amount);
 
 }
 
 const update = () => {
-    location.direction = directions[directionIndex];
+    location.direction = cardinals[directionIndex];
     location.location = [x,y];
+    wayPointLocation = [waypointX,waypointY];
 }
 
 const useDirections = directions => {
@@ -37,9 +144,6 @@ const useDirections = directions => {
         }
         update();
     })
-
-    console.log(location)
-
     return [x,y];
    
 }
@@ -68,7 +172,7 @@ const moveDirection = ([direction, amount])  => {
 
 const faceDirection = ([turn, amount]) => {
 
-    let moves = (amount % 90) + 1;
+    let moves = (amount / 90);
 
     if (turn == "R") {
         directionIndex = directionIndex + moves;
@@ -78,21 +182,30 @@ const faceDirection = ([turn, amount]) => {
         directionIndex -= moves;
     }
 
+    if (directionIndex < 0) {
+        directionIndex += 4;
+    }
+
+    if (directionIndex > 3) {
+        directionIndex -= 4;
+    }
 
 }
 
 const makeDirections = data => {
-    return data.map(item => [item.substr(0, 1), item.substr(1)])
+    return data.map(item => (
+        [item.substr(0, 1), item.substr(1)]
+    ))
 }
 
 
 
-const run = async () => {
-    const answer = await findAnswer(``);
-    console.log(answer);
-}
+// const run = async () => {
+//     const answer = await findAnswer(``);
+//     console.log(answer);
+// }
 
-run();
+// run();
 
 module.exports = {
     findAnswer
--- 2020/day_12/test.js
+++ 2020/day_12/test.js
@@ -1,20 +1,39 @@
 const {findAnswer} = require('./script');
 
-describe('day8', () => {
+describe('day12', () => {
 
-    describe.only('part a',  () => {
+    describe('part a',  () => {
 
         test("example", async () => {
             const answer = await findAnswer(`test`);
             expect(answer[0]).toBe(25);
+        }) 
+
+        test("real not 1461", async () => {
+            const answer = await findAnswer(``);
+            expect(answer[0]).not.toBe(1461);
+        })
+
+        test("real not 731", async () => {
+            const answer = await findAnswer(``);
+            expect(answer[0]).not.toBe(731);
+        })
+        
+        
+        test("real not 599", async () => {
+            const answer = await findAnswer(``);
+            expect(answer[0]).not.toBe(599);
+        })
+
+        test("real not 230", async () => {
+            const answer = await findAnswer(``);
+            expect(answer[0]).not.toBe(230);
         })
 
         test("real", async () => {
             const answer = await findAnswer(``);
-            expect(answer[0]).toBe(230);
+            expect(answer[0]).toBe(1496);
         })
-
-        
 
     })
 
@@ -22,15 +41,23 @@ describe('day8', () => {
 
         test("example", async () => {
             const answer = await findAnswer(`test`);
-            expect(answer[1]).toBe(8);
+            expect(answer[1]).toBe(286);
+        })
+
+
+        test("real not 72485", async () => {
+            const answer = await findAnswer(``);
+            expect(answer[1]).not.toBe(72485);
         })
 
 
         test("real", async () => {
             const answer = await findAnswer(``);
-            expect(answer[1]).toBe(56693912375296);
+            expect(answer[1]).toBe(63843);
         })
 
+
+        
     });
 
 
